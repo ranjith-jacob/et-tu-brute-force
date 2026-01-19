32a3b9ca4e6fe726e49c57c1a00575446144336a
ranjith.j <ranjithjacob.work@gmail.com>
2026-01-19 09:35:08 +0000

    builds Identities index page

--- controllers/identities.js
+++ controllers/identities.js
@@ -3,4 +3,18 @@ const router = express.Router();
 
 const Identity = require("../models/identity");
 
+router.get("/", async (req, res) => {
+    try {
+        const getAllIdentities = await Identity.find({}).populate("owner");
+        console.log("all of the Identities", getAllIdentities);
+        // res.send("Identities index page");
+        res.render("./identities/index.ejs", {
+            identities: getAllIdentities,
+        });
+    } catch (error) {
+        console.log(error); // consider error.message here
+        res.redirect("/");
+    }
+});
+
 module.exports = router;
