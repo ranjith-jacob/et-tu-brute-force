ac146b795c213c4b663d0136332fb895ad5c145b
ranjith.j <ranjithjacob.work@gmail.com>
2026-01-20 07:03:04 +0000

    fixes html checkbox error and now stores unchecked values

--- controllers/identities.js
+++ controllers/identities.js
@@ -25,7 +25,12 @@ router.get("/new", async (req, res) => {
 router.post("/", async (req, res) => {
     // console.log("Who is the user", req.session.user._id);
     req.body.owner = req.session.user._id;
+    req.body.pwdReused = req.body.pwdReused === "on" ? true : false;
+    req.body.storedInPwdManager = req.body.storedInPwdManager === "on" ? true : false;
+    req.body.mfaPresent = req.body.mfaPresent === "on" ? true : false;
+    req.body.pwnedStatus = req.body.pwnedStatus === "on" ? true : false;
     console.log("Form data received", req.body);
+    // req.body.pwdReused = req.body.pwdReused === "on" ? true : false;
     await Identity.create(req.body);
     res.redirect("/identities");
 });
