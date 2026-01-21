a0a3e059a208ee8f8a557f4f8b15bc51cef81c6e
ranjith.j <ranjithjacob.work@gmail.com>
2026-01-21 14:50:53 +0000

    creates structure for express.static mw and links css

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const identitiesController = require("./controllers/identities");
 
 const port = process.env.PORT ? process.env.PORT : "3000";
 
+const path = require("path");
+
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
